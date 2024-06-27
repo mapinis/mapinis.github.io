4dba7bf405754ee8815dfb377b7acb33a59e76af
Mark Apinis <mapinis2001@gmail.com>
2024-06-27 19:04:55 -0400

    Test Apinis.org

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      Apinis.org
+      Welcome to Apinis.org
     </main>
   );
 }
