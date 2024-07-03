638759e16e98f953bf6a3c6c1eb54eb2f8fac8b4
Mark Apinis <mapinis2001@gmail.com>
2024-07-03 19:20:03 -0400

    Dynamic design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import "./page.module.css";
+import styles from "./page.module.css";
 
 export default function Page() {
   return (
-    <h2>About Me</h2>
+    <div id={styles.content}>
+      <div id={styles.photoFrame}>
+        <img src="headshot.jpeg" alt="Mark Apinis headshot" />
+        <div>
+          <p><b>Contact Me:</b></p>
+          <p><a href="mailto:mark@apinis.org">mark@apinis.org</a></p>
+        </div>
+      </div>
+      <div id={styles.text}>
+        <h2>About Me</h2>
+      </div>
+    </div>
   );
 }
