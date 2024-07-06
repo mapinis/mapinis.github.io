611aa5fa637be12736e52af7e6e5619b4a73c837
Mark Apinis <mapinis2001@gmail.com>
2024-07-05 20:04:40 -0400

    Added socials, color work, formatted css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,11 @@ export default function Page() {
     <>
       <div id={styles.content}>
         <div id={styles.photoFrame}>
-          <img src="headshot.jpeg" alt="Mark Apinis headshot" />
+          <img src="headShot.jpeg" alt="Mark Apinis head shot" />
           <div>
             <p><b>Contact Me:</b></p>
             <p><a href="mailto:mark@apinis.org">mark@apinis.org</a></p>
+            <span id={styles.socials}><a href="https://github.com/mapinis">Github</a> · <a href="https://www.linkedin.com/in/mapinis/">LinkedIn</a></span>
           </div>
         </div>
         <div id={styles.text}>
