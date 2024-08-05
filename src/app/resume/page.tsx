8f6abc16b52cd03791dc5e8f8e3501d28a76ca64
Mark Apinis <mapinis2001@gmail.com>
2024-08-05 14:09:28 -0400

    Resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,25 +1,12 @@
-"use client";
-
 import styles from "./page.module.css";
-import { Document, Page, pdfjs } from "react-pdf";
-
-// react-pdf layers
-import 'react-pdf/dist/esm/Page/TextLayer.css';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
 
 export default function ResumePage() {
 
   return (
-    <div id={styles.resumeContainer}>
-      <Document className={styles.document} file="/resume-no-cell.pdf">
-        <Page className={styles.page} pageNumber={1} width={document.getElementById("main")?.clientWidth ?? document.getElementById("main")?.scrollWidth}/>
-      </Document>
+    <div id={styles.resume}>
+      <object data="/resume-no-cell.pdf#view=FitV&toolbar=0" type="application/pdf" width="100%" height="100%">
+        <p>Click <a href="/resume-no-cell.pdf" target="_blank">here</a> to open resumé PDF.</p>
+      </object>
     </div>
   )
 }
