0f0f125191ea2e636c85b59c49cd5eb2735e05f0
Mark Apinis <mapinis2001@gmail.com>
2024-08-06 21:00:03 -0400

    Added another project, slight formatting change and restructure

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,8 +4,8 @@ export default function Page() {
 
   return (
     <div id={styles.resume}>
-      <object data="/resume-no-cell.pdf#view=FitV&toolbar=0" type="application/pdf" width="100%" height="100%">
-        <p>Click <a href="/resume-no-cell.pdf" target="_blank">here</a> to open resumé PDF.</p>
+      <object data="/documents/resume-no-cell.pdf#view=FitV&toolbar=0" type="application/pdf" width="100%" height="100%">
+        <p>Click <a href="/documents/resume-no-cell.pdf" target="_blank">here</a> to open resumé PDF.</p>
       </object>
     </div>
   )
